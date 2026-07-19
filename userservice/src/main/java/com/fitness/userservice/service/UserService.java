b02afce5727fd7d592646d0b7cdfcb40fefa22b1
Shubham Choubey <shubhamchoubey115@gmail.com>
2026-07-19 22:05:33 +0530

    required arguments constructor

--- userservice/src/main/java/com/fitness/userservice/service/UserService.java
+++ userservice/src/main/java/com/fitness/userservice/service/UserService.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class UserService {
 
+    //if we use @requiredargumetsconstructor then all final fields will be autowired
     UserRepository repository;
 
     public UserResponse registerNewUser(RegisterRequest request) {
