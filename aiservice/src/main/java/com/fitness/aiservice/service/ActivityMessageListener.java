8126c47d17e54489bd50957911c09186f746837e
Shubham Choubey <shubhamchoubey115@gmail.com>
2026-07-25 23:12:25 +0530

    saving ai response to mongodb

--- aiservice/src/main/java/com/fitness/aiservice/service/ActivityMessageListener.java
+++ aiservice/src/main/java/com/fitness/aiservice/service/ActivityMessageListener.java
@@ -19,8 +19,8 @@ public class ActivityMessageListener {
     @RabbitListener(queues = "activity.queue")
     public void processActivity(Activity activity) {
         log.info("Received activity for processing: {}", activity.getId());
-        log.info("Generated Recommendation: {}", aiService.generateRecommendation(activity));
-        //Recommendation recommendation = aiService.generateRecommendation(activity);
-        //recommendationRepository.save(recommendation);
+        //log.info("Generated Recommendation: {}", aiService.generateRecommendation(activity));
+        Recommendation recommendation = aiService.generateRecommendation(activity);
+        recommendationRepository.save(recommendation);
     }
 }
