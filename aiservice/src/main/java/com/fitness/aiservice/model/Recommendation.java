bd02ebaf7dabf41363026301f7d2316d459fcd40
Shubham Choubey <shubhamchoubey115@gmail.com>
2026-07-25 23:13:39 +0530

    updated db details

--- aiservice/src/main/java/com/fitness/aiservice/model/Recommendation.java
+++ aiservice/src/main/java/com/fitness/aiservice/model/Recommendation.java
@@ -11,7 +11,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import java.time.LocalDateTime;
 import java.util.List;
 
-@Document(collection = "recommendations")
+@Document(collection = "recommendation")
 @Data
 @Builder
 @AllArgsConstructor
--- aiservice/src/main/java/com/fitness/aiservice/service/RecommendationService.java
+++ aiservice/src/main/java/com/fitness/aiservice/service/RecommendationService.java
@@ -3,17 +3,22 @@ package com.fitness.aiservice.service;
 import com.fitness.aiservice.model.Recommendation;
 import com.fitness.aiservice.repository.RecommendationRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class RecommendationService {
     private final RecommendationRepository recommendationRepository;
 
     public List<Recommendation> getUserRecommendation(String userId) {
-        return recommendationRepository.findByUserId(userId);
+        log.info("userId is {}",userId);
+        List<Recommendation> recommendations = recommendationRepository.findByUserId(userId);
+        log.info("recomendation saved in db is {}", recommendations);
+        return recommendations;
     }
 
     public Recommendation getActivityRecommendation(String activityId) {
